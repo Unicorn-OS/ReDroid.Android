e85b8c3c72b2b2b53b8b10f17a1aafca5315184a
DarkStar☀ <61005029+Twin-Flame@users.noreply.github.com>
2023-12-01 06:37:02 -0600

    Update ADB push.sh

--- .graph/Google Play/extract to/Open GApps/ADB push.sh
+++ .graph/Google Play/extract to/Open GApps/ADB push.sh
@@ -1,7 +1,13 @@
+# https://groups.google.com/g/android-rpi/c/xb2fwTQbUYw
 dev="localhost:5555"
 
 adb -s $dev root
 
 sudo adb -s $dev push open_gapps-x86_64-11.0-pico-20220503 /open_gapps-x86_64-11.0-pico-20220503
 
-# https://groups.google.com/g/android-rpi/c/xb2fwTQbUYw
+# Extract
+adb -s $dev shell
+
+mv -r open_gapps-x86_64-11.0-pico-20220503/* /system/
+
+rmdir open_gapps-x86_64-11.0-pico-20220503
