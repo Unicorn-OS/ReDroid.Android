093d587cffa094836f283e69a4020a1895f0ad22
DarkStar☀ <61005029+Twin-Flame@users.noreply.github.com>
2023-12-01 06:43:47 -0600

    Update and rename ADB push.sh to ADB GApps install.sh

--- .graph/Google Play/extract to/Open GApps/ADB GApps install.sh
+++ .graph/Google Play/extract to/Open GApps/ADB GApps install.sh
@@ -11,3 +11,12 @@ adb -s $dev shell
 mv -r open_gapps-x86_64-11.0-pico-20220503/* /system/
 
 rmdir open_gapps-x86_64-11.0-pico-20220503
+
+exit
+
+# Snapshot
+docker stop myandroid0
+docker commit myandroid0 my/opengapps:pico
+
+# run
+docker run -itd --name gapps --privileged -v ~/data:/data -p 5555:5555 my/opengapps:pico
