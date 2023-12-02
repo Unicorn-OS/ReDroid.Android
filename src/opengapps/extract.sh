4420b349bdf07ff554b6881bd0e14aeaed16d427
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-12-02 04:37:27 -0600

    almost

--- src/opengapps/extract.sh
+++ src/opengapps/extract.sh
@@ -2,6 +2,7 @@ to=open_gapps
 zip=open_gapps-x86-11.0-pico-20220503.zip
 this=`pwd`
 app=$this/$to/app
+common=$this/$to/common
 
 unzip_archive(){
   unzip -d $to $zip
@@ -37,42 +38,97 @@ each_dir(){
   extract_lz_archives
 }
 
-delete_empty(){
+rm_empty(){
   rmdir nodpi/priv-app
   rmdir nodpi
 }
 
-inner_app(){
+remove_empty_recursive(){
+  # https://unix.stackexchange.com/questions/46322/how-can-i-recursively-delete-empty-directories-in-my-home-directory
+  find . -type d -empty -print
+}
+
+is_app(){
+  rsync -a --remove-source-files app/* $app
+  # mv app/* $app
+  rmdir app
+}
+
+is_priv_app(){
+  rsync -a --remove-source-files priv-app/* $app
+  # mv priv-app/* $app
+  rmdir priv-app
+}
+
+move_to_app(){
+  cd nodpi
+  for f in *;do
+    if [[ "$f" == "priv-app" ]];then
+      ls
+      is_priv_app
+    else
+      echo not private
+      is_app
+    fi
+  done
+  cd ~-
+  echo
+}
+
+move_to_common(){
+  # https://unix.stackexchange.com/questions/127712/merging-folders-with-mv
+  rsync -a --remove-source-files common/* $common
+}
+
+contains_inner_app(){
   cd $1
 
   for f in *;do
     if [[ "$f" == "nodpi" ]];then
+      # pwd
+      # move_to_app $1
+      # pwd
+      # rmdir nodpi
+      echo
+    else
+      # debug_no_dpi
+      ls
       pwd
-      mv nodpi/priv-app/* $app
-      echo true
+      move_to_common
+      remove_empty_recursive
     fi
   done
-  cd ..
+  cd ~-
 }
 
 extract_to_app(){
-  mkdir -p $app
+  mkdir -p $app $common
 
   cd $this/$to/Core
   for f in */;do
     # echo $f
-    inner_app $f
+    contains_inner_app $f
+    # pwd
+    # rmdir $f
     # mv $f/nodpi/priv-app/*
   done
 }
 
-move_up_one(){
-  echo
+bac(){
+  decompress
+  tar -cf open_gapps.tar open_gapps
 }
 
+restore(){
+  clean
+  tar -xf open_gapps.tar
+}
+
+
 main(){
-  decompress
-  # extract_to_app
+  # decompress
+  restore
+  extract_to_app
 }
 
 main
