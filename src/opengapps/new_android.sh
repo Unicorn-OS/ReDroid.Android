98d577a8bbcf811cae11e2fa15aa1b711fb1a9e8
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-12-02 07:00:58 -0600

    gapps

--- src/opengapps/new_android.sh
+++ src/opengapps/new_android.sh
@@ -1,10 +1,33 @@
+future_container=gappstest
 container=newandroid
 dev=localhost:5555
 
-docker stop $container
+remove_competing_port(){
+    docker stop $future_container
+    docker rm --force $future_container
+}
 
-docker run -itd --privileged --pull always -v ~/data11:/data -p 5555:5555 --name $container redroid/redroid:11.0.0-latest
 
-adb kill-server
+launch(){
+    docker stop $container
 
-adb connect $dev
+    docker run -itd --privileged --pull always -v ~/data11:/data -p 5555:5555 --name $container redroid/redroid:11.0.0-latest
+}
+
+reset_connection(){
+    adb kill-server
+    adb connect $dev
+}
+
+debug(){
+    docker ps --all
+}
+
+main(){
+    remove_competing_port
+    launch
+    reset_connection
+    debug
+}
+
+main
