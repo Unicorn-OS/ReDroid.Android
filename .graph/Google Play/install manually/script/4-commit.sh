735f4704cd48dfd342da50775c256e1b6f8f5e60
DarkStar☀ <61005029+Twin-Flame@users.noreply.github.com>
2023-12-02 02:41:23 -0600

    Update 4-commit.sh

--- .graph/Google Play/install manually/script/4-commit.sh
+++ .graph/Google Play/install manually/script/4-commit.sh
@@ -1,11 +1,12 @@
-from=androidbase
+container=redroid11
+newimage=my/opengapps
 
 # Snapshot
-docker stop androidbase
-docker commit androidbase my/opengapps:stock
-docker rm androidbase
+docker stop $container
+docker commit $container $newimage
+docker rm $androidbase
 
 # run
-docker run -itd --name androidgapps --privileged -v ~/data:/data -p 5555:5555 my/opengapps:stock
+docker run -itd --name gapps --privileged -v ~/data:/data -p 5555:5555 $newimage
 
 docker ps --all
